2d16121c664655e7adb6121b2d67db2e45e86279
masp1234 <mart2c04@stud.kea.dk>
2024-04-22 17:07:24 +0200

    remove containers script and variable files added

--- exercises/08b_migrating_data/scripts/mysql/setup-mysql.sh
+++ exercises/08b_migrating_data/scripts/mysql/setup-mysql.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-CONTAINER_NAME=mysql-test-db
-DB_PASSWORD=123123
-DB_USER=root
-HOST_PORT=3306
-MYSQL_PORT=3306
+source ./variables.sh
 
 docker run --name $CONTAINER_NAME -e MYSQL_ROOT_PASSWORD=$DB_PASSWORD -p $HOST_PORT:$MYSQL_PORT -d mysql
+
 echo "Starting $CONTAINER_NAME..."
+
 sleep 20
+
 echo "Container started. Creating database..."
+
 docker exec -i $CONTAINER_NAME mysql -u$DB_USER -p$DB_PASSWORD -P$MYSQL_PORT< "./setup.sql"
+
 echo "Done!"
---
+++ exercises/08b_migrating_data/scripts/remove-containers.sh
@@ -0,0 +1,10 @@
+source ./mysql/variables.sh
+
+docker rm -f $CONTAINER_NAME
+
+source ./mongodb/variables.sh
+
+docker rm -f $CONTAINER_NAME
+
+
+
